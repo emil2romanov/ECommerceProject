377bd105ed4d435b7f8073325972c155bc21838f
Emil Romanov <eromanov8@gmail.com>
2023-10-10 15:18:54 +0300

    creating country entity

---
+++ src/main/java/com/romanov/ecommerce/entity/Country.java
@@ -0,0 +1,23 @@
+package com.romanov.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "country")
+@Data
+public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "code")
+    private String code;
+
+    @Column(name = "name")
+    private String name;
+
+    // TODO: Set up one-to-many with states
+}
